b76da553df57d1987dfeac981a428087eb6a3f06
saracasa <sarina.casadio@gmail.com>
2026-02-09 12:59:32 +0100

    refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.storage.StorageLevel
 import java.nio.file.{Files, Paths, StandardOpenOption}
 import java.nio.charset.StandardCharsets
 
@@ -7,7 +8,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     //2 argomenti: path di input e output
     if (args.length < 2) {
-      System.err.println("Usage: Main <input-path> <output-folder>")
+      System.err.println("Usage: Main <input-path> <output-folder> [partitions] [workers]")
       System.exit(1)
     }
     val filename = args(0)
@@ -22,12 +23,12 @@ object Main {
       .getOrCreate()
 
     import spark.implicits._
-
     spark.sparkContext.setLogLevel("ERROR") // Rimuove log (escluso errori)
 
     // tempo iniziale
     val t0 = System.nanoTime()
 
+    //Caricamento RDD
     val data = spark.read
       .option("header", value = true) // Prima riga del csv non contiene dati
       .csv(filename)
@@ -37,20 +38,18 @@ object Main {
     val cleanedData = data.map(row => {
       val lat = row.getAs[String]("latitude").toDouble
       val lon = row.getAs[String]("longitude").toDouble
-      val date = row.getAs[String]("date").substring(0, 10) // Prende solo YYYY-MM-DD (finestra temporale di 1 giorno)
+      val date = row.getAs[String]("date").substring(0, 10) // YYYY-MM-DD
 
-      // Arrotondo alla prima cifra decimale
+      // Arrotondo prima cifra decimale
       val latRound = BigDecimal(lat).setScale(1, BigDecimal.RoundingMode.HALF_UP).toDouble
       val lonRound = BigDecimal(lon).setScale(1, BigDecimal.RoundingMode.HALF_UP).toDouble
 
       ((latRound, lonRound), date)
     }).distinct() // Rimuove duplicati nello stesso giorno
 
-    // 1.2. Formato del risultato dell’analisi
+    // 1.2. Analisi delle co-occorrenze
     val events = cleanedData.toDF("location", "date")
       .repartition(numPartitions) // Trasformo l'RDD in un DataFrame e lo partiziona in cluster,
-    //.persist(org.apache.spark.storage.StorageLevel.MEMORY_AND_DISK)// Rendo i dati persistenti per evitare ricalcoli durante il join
-
 
     // Analisi di co-occorrenza (Map-Reduce Distributed Task)
     val results = events.as("a")
@@ -58,8 +57,6 @@ object Main {
       .filter(col("a.location") < col("b.location"))
       .groupBy(col("a.location").as("loc_a"), col("b.location").as("loc_b"))
       .agg(count("*").as("count"), collect_list("a.date").as("dates"))
-      //.orderBy(desc("count"))
-      //.persist() // Evita ricalcoli per .write e .first()
 
     val topResult = results.rdd.max()(Ordering.by(_.getAs[Long]("count"))) // Prendo risultato con massimo numero di occorrenze
 
@@ -71,36 +68,27 @@ object Main {
 
     val datesList = topResult.getList[String](3).toArray.map(_.toString).sorted // Estraggo la lista delle date e le ordino in modo crescente
     datesList.foreach(println)
-    //println(s"Le date della co-occorrenza più frequente sono ${datesList.length}")
 
-    println(s"Tempo di esecuzione: $durationSeconds secondi")
-    println(s"Coppia Max: (${topResult.get(0)}, ${topResult.get(1)}) con ${topResult.get(2)} occorrenze")
-
-    // Salva nel txt
+    //Output
     val locA = topResult.get(0)
     val locB = topResult.get(1)
     val datesSorted = topResult.getList[String](3).toArray.map(_.toString).sorted
 
-    val outputSnippet = s"(($locA),($locB)) | ${datesSorted.take(10).mkString(" | ")}..."
-    val logEntry = s"""{"workers": "$workers", "partitions": $numPartitions, "time_seconds": $durationSeconds, "output_snippet": "$outputSnippet"}\n${"-" * 40}\n"""
+    println(s"(($locA), ($locB))")
+    datesSorted.foreach(println)
+    println(s"Tempo di esecuzione: $durationSeconds secondi")
 
-    val directory = Paths.get(outputPath)
-    val filePath = directory.resolve("results.txt")
-
-    try {
-      if (!Files.exists(directory)) Files.createDirectories(directory)
-
-      Files.write(
-        filePath,
-        logEntry.getBytes(StandardCharsets.UTF_8),
-        StandardOpenOption.CREATE,
-        StandardOpenOption.APPEND
-      )
-      println(s"Esperimento salvato in: ${filePath.toAbsolutePath}")
-    } catch {
-      case e: Exception => println(s"Errore salvataggio: ${e.getMessage}")
-    }
+    // Salva nel txt
+    saveResults(outputPath, locA, locB, datesSorted, durationSeconds, numPartitions, workers)
 
     spark.stop()
+  }
+
+  def saveResults(path: String, lA: Any, lB: Any, dates: Array[String], time: Double, parts: Int, workers: String): Unit = {
+    val logEntry = s"""{"workers": "$workers", "partitions": $parts, "time_seconds": $time, "pair": "($lA,$lB)"}\n"""
+    val directory = Paths.get(path)
+    if (!Files.exists(directory)) Files.createDirectories(directory)
+    Files.write(directory.resolve("results.txt"), logEntry.getBytes(StandardCharsets.UTF_8),
+      StandardOpenOption.CREATE, StandardOpenOption.APPEND)
   }
 }
